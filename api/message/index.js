922fd1bc347771e4285b7f69e0357f53b4266e42
macavall <43223084+macavall@users.noreply.github.com>
2023-10-02 13:13:53 -0500

    Update index.js

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,10 @@
 module.exports = async function (context, req) {
+    await sleep(20000);
     context.res.json({
         text: "Hello from the Amazing API!!!!"
     });
 };
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
